f1bbd455be7e060965e06354fcf22be9f3735b08
DamiKasa <rovellodami22@gmail.com>
2022-10-20 16:40:23 -0300

    Pagina de works terminada.

--- assets/js/thanks.js
+++ assets/js/thanks.js
@@ -1,25 +1,25 @@
-const dir = "https://dog.ceo/api/breeds/image/random"
+// const dir = "https://dog.ceo/api/breeds/image/random"
 
-async function dog() {
-    const response = await fetch(dir)
-    const data = await response.json()
-    console.log(data)
+// async function dog() {
+//     const response = await fetch(dir)
+//     const data = await response.json()
+//     console.log(data)
 
-    let img = document.createElement("img")
+//     let img = document.createElement("img")
 
-    let doggie = data.message
+//     let doggie = data.message
 
-    img.setAttribute("src", doggie)
+//     img.setAttribute("src", doggie)
 
-    let division = document.getElementById("imgDog")
+//     let division = document.getElementById("imgDog")
 
-    division.appendChild(img)
+//     division.appendChild(img)
 
-    function removeBtn() {
-        var elem = document.getElementById('boton')
-        elem.parentNode.removeChild(elem);
-        return false;
-    }
+//     function removeBtn() {
+//         var elem = document.getElementById('boton')
+//         elem.parentNode.removeChild(elem);
+//         return false;
+//     }
 
-    removeBtn()
-}
+//     removeBtn()
+// }
